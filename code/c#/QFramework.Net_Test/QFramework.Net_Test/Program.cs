490e7cd3371fffdbe0fbd19e6cd4942dc875d43d
DealiAxy <admin@deali.cn>
2021-09-14 15:57:53 +0800

    init

---
+++ code/c#/QFramework.Net_Test/QFramework.Net_Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QFramework;
+
+namespace QFramework.Net_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QFramework.QCls_QApplication QApp = new QCls_QApplication();
+
+            QApp.QApp_Version_Major = 1;
+            QApp.QApp_Version_Minor = 2;
+            QApp.QApp_Version_Sub = 1;
+            QApp.QApp_Version_Update = 1;
+
+            Console.WriteLine(QApp.QApp_Version);
+            Console.ReadKey();
+        }
+    }
+}
